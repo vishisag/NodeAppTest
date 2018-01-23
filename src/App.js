c5bd2ba705c79fbc052f184b17fa6d52b840833b
Sangani, Viral (US - Hyderabad) <vsangani@deloitte.com>
2018-01-23 15:30:39 +0530

    simple teest change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Hi Viral
+          Hi Viral This is a test
         </p>
       </div>
     );
