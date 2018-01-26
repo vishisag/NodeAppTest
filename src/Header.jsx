e85ee5539f8ff1d5d606dc3b6434200835fe9597
vishisag <veeralsangani90@gmail.com>
2018-01-26 16:49:45 +0530

    testcommit

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './css/header.css';
+
+class Header extends Component{
+    render(){
+        return(
+            <div>
+                <body>
+                <h3> Welcome to the test app </h3>
+                </body>
+                </div>
+        );
+    }
+}
+
+export default Header 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Header from './Header.jsx';
+import Login from './Login.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header />,document.getElementById('root'));
+ReactDOM.render(<Login />,document.getElementById('root'));
 registerServiceWorker();
